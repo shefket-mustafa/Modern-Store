43e6c5756665fa8dbdd267eab4aa6346dea28427
Shefket Mustafa <miracle_1@abv.bg>
2025-11-05 22:53:41 +0200

    Login and Register components logic done, Zod validation added and React hook form used for form handling

--- Modern-Store/client/src/pages/Register.tsx
+++ Modern-Store/client/src/pages/Register.tsx
@@ -1,10 +1,48 @@
+import { useForm } from "react-hook-form";
+import { registerSchema, type RegisterSchemaType } from '../lib/zod/registerSchema';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router";
+
 export default function Register() {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: {errors, isSubmitting}
+  } = useForm<RegisterSchemaType>({resolver: zodResolver(registerSchema)})
+
+  const handleRegister = async(data: RegisterSchemaType) =>{
+    const {username, email, password} = data;
+
+    try{
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({username, email, password})
+      })
+
+      if(!res.ok) {
+        setError("root", {message: "Register failed - Internal issue"})
+      }
+
+      navigate("/auth/login")
+
+    }catch(err){
+      if(err instanceof Error){
+        setError("root", {message: err.message})
+      }else {
+        setError("root", {message: "Failed to register!"})
+      }
+    }
+  }
 
    
           return (
     <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-gray-50 to-gray-100">
       <form
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit(handleRegister)}
         className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-sm border border-gray-100"
       >
         <h2 className="text-3xl font-semibold text-center mb-6 text-gray-800">
@@ -12,68 +50,76 @@ export default function Register() {
         </h2>
        
 
-        {/* {error && (
+        {errors.root && (
           <p className="text-red-500 text-sm mb-3 bg-red-50 p-2 rounded-lg text-center">
-            {error}
+            {errors.root.message}
           </p>
-        )} */}
+        )}
 
          <div className="mb-5">
           <label className="block text-gray-700 mb-2 text-sm">Username</label>
           <input
             type="text"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            {...register("username")}
             required
             className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500 transition"
           />
+          {errors.username && (
+            <p className="text-red-500 text-sm mb-3 bg-red-50 p-2 rounded-lg text-center">{errors.username.message}</p>
+          )}
         </div>
 
         <div className="mb-5">
           <label className="block text-gray-700 mb-2 text-sm">Email</label>
           <input
             type="email"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            {...register("email")}
             required
             className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500 transition"
           />
+           {errors.email && (
+            <p className="text-red-500 text-sm mb-3 bg-red-50 p-2 rounded-lg text-center">{errors.email.message}</p>
+          )}
         </div>
 
         <div className="mb-6">
           <label className="block text-gray-700 mb-2 text-sm">Password</label>
           <input
             type="password"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
+            {...register("password")}
             required
             className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500 transition"
           />
+           {errors.password && (
+            <p className="text-red-500 text-sm mb-3 bg-red-50 p-2 rounded-lg text-center">{errors.password.message}</p>
+          )}
         </div>
 
           <div className="mb-6">
           <label className="block text-gray-700 mb-2 text-sm">Confirm Password</label>
           <input
             type="password"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
+           {...register("confirmPassword")}
             required
             className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500 transition"
           />
+           {errors.confirmPassword && (
+            <p className="text-red-500 text-sm mb-3 bg-red-50 p-2 rounded-lg text-center">{errors.confirmPassword.message}</p>
+          )}
         </div>
 
         <button
           type="submit"
-        //   disabled={loading}
+          disabled={isSubmitting}
           className="w-full bg-orange-500 hover:bg-orange-600 disabled:bg-orange-300 text-white font-semibold py-2.5 rounded-lg shadow-sm transition duration-150"
         >
-          {/* {loading ? "Signing in..." : "Sign In"} */}
+          {isSubmitting ? "Signing in..." : "Sign In"}
         </button>
 
         <p className="text-center text-sm text-gray-500 mt-6">
           Don’t have an account?{" "}
-          <a href="/register" className="text-orange-500 hover:underline">
-            Sign Up
+          <a href="/auth/login" className="text-orange-500 hover:underline">
+            Already signed?
           </a>
         </p>
       </form>
