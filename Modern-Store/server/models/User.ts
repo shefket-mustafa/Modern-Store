6b67c22942c33e4f7ce28938942d31e592a21e82
shefket-mustafa <miracle_1@abv.bg>
2025-11-19 00:52:26 +0200

    Fixing types and imports for the server deployment

--- Modern-Store/server/models/User.ts
+++ Modern-Store/server/models/User.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from 'mongoose';
-import { UserType } from '../../client/types';
+import { UserType } from '../serverTypes';
+
 
 
 const userSchema = new Schema<UserType>({
--- Modern-Store/server/serverTypes.ts
+++ Modern-Store/server/serverTypes.ts
@@ -2,3 +2,12 @@ import { Request } from 'express';
 
 export type ReqUser = { id?: string; role?: 'user' | 'admin' };
 export type AdminRequest = Request & { user?: ReqUser };
+
+export type UserType = {
+  _id: string
+    username: string;
+    email: string;
+    password: string
+    // role indicates authorization level; 'user' by default, 'admin' for administrators
+    role?: 'user' | 'admin'
+}
